1c9711e6c9f6223368dbbc12f72ead20d5213eb1
alihaidershaker <ak6222698@gmail.com>
2024-10-28 23:30:10 +0300

    Add files via upload

--- data/BioData.js
+++ data/BioData.js
@@ -1,20 +1,20 @@
 // date: 1 Aug, 2022
 const bioData = [
     {
-        name: 'nxt lnk',
-        username: 'nxtlnk.vercel.app',
+        name: 'ali haider shaker',
+        username: 'dexter',
         url: 'https://nxtlnk.vercel.app',
         titleImg: true,
-        avatar: '/avatar.png',
+        avatar: '/AQADcuYxG71mAAFJfQ.png',
         nftAvatar: true,
         description: 'Custom bio links for creatives who love coding. Made this first to use for my own weblinks vjy.me/lnk. ',
         descShow: true,
         subdesc: 'Use this template and deploy on vercel for free',
         subdescShow: true,
         newProductUrl: 'https://3dicons.co',
-        newProduct: true,
-        author: 'realvjy',
-        authorURL: 'https://twitter.com/realvjy',
+        newProduct: false,
+        author: 'ali haider',
+        authorURL: 'https://twitter.com/',
         footerText: 'made by'
     },
 ];
