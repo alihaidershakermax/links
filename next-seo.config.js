5f9ed58765193b7249b900563242d7d72a9ad1a3
alihaidershaker <ak6222698@gmail.com>
2024-10-29 13:59:05 +0300

    Update next-seo.config.js

--- next-seo.config.js
+++ next-seo.config.js
@@ -3,7 +3,7 @@ export default {
         type: 'website',
         locale: 'en_IE',
         title: 'biolinks by alihaider',
-        url: 'https://vjy.me',
+        url: 'https://alihaiderxcx.bio.link/',
         description: 'Custom bio links for creatives who love coding. Made this first to use for my own weblinks vjy.me/lnk.',
         locale: 'en_EN',
         keywords: 'designer, product designer, illustrator, indian designer, vijay verma, realvjy, illlustrations, indian illustrator, ui designer india, sushi design system, zomato designer, 3dicons, uiprint, vijay realvjy, indian ui designer, design india, overlayz, nft art',
@@ -11,7 +11,7 @@ export default {
             {
                 width: 1200,
                 height: 630,
-                url: `https://envs.sh/_2V.jpg`,
+                url: `https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhQn7L7nU_e1DatALO8dZQIzDzMcHMCf_9SZP9_mp6BT0PTDcpyWBLm-rPgRH7uQ0oAau3pp7vNgU54IySqSaEnB6hneEwh9x0ZAAnHJ4EZDQrKh7i6q1QYnbT72zqvXYos3cR0mJR4F0dL/w1920-h1080-c/night-king-and-jon-snow-game-of-thrones-season-8-uhdpaper.com-4K-38.jpg`,
             },
         ],
         site_name: 'ALI HAIDER',
