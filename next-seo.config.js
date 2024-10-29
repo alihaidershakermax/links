db9561ad3f17b6a17fb0120dd12d2b36da139719
alihaidershaker <ak6222698@gmail.com>
2024-10-29 15:02:09 +0300

    Update next-seo.config.js

--- next-seo.config.js
+++ next-seo.config.js
@@ -2,8 +2,8 @@ export default {
     openGraph: {
         type: 'website',
         locale: 'en_IE',
-        title: 'biolinks by alihaider',
-        url: 'https://alihaiderxcx.bio.link/',
+        title: 'ALIHAIDERSHAKER',
+        url: 'https://alihaidershaker.vercel.app/',
         description: '',
         locale: 'en_EN',
         keywords: '',
@@ -11,7 +11,7 @@ export default {
             {
                 width: 1200,
                 height: 630,
-                url: `https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhQn7L7nU_e1DatALO8dZQIzDzMcHMCf_9SZP9_mp6BT0PTDcpyWBLm-rPgRH7uQ0oAau3pp7vNgU54IySqSaEnB6hneEwh9x0ZAAnHJ4EZDQrKh7i6q1QYnbT72zqvXYos3cR0mJR4F0dL/w1920-h1080-c/night-king-and-jon-snow-game-of-thrones-season-8-uhdpaper.com-4K-38.jpg`,
+                url: ``,
             },
         ],
         site_name: 'ALI HAIDER',
